490a3e78bf7bf41654c3aac2c289a825eaed9420
charlotte <117921625+charlottekruzic@users.noreply.github.com>
2026-02-04 18:34:15 +0100

    feat: add volume for raw json files

--- databricks/0_setup/create_strava_catalog.py
+++ databricks/0_setup/create_strava_catalog.py
@@ -6,3 +6,9 @@
 # MAGIC CREATE SCHEMA IF NOT EXISTS strava_catalog.bronze;
 # MAGIC CREATE SCHEMA IF NOT EXISTS strava_catalog.silver;
 # MAGIC CREATE SCHEMA IF NOT EXISTS strava_catalog.gold;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- Create volume for raw JSON files
+# MAGIC CREATE VOLUME IF NOT EXISTS strava_catalog.raw.json_files
